bda11e4aa4f501be2ac3b39253c8d28c329eb560
Бояршинов Константин <costya74@bk.ru>
2022-02-06 19:26:30 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1100;
+        int mili = 1;
+        int payment = 20;
+        int amountMili = ticket * mili / payment;
+        System.out.println(" Количество милей составило " + amountMili);
+    }
+}
